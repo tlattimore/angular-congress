b23fd4c88807d834b9d5dc102080e8171e4c4e46
tlattimore <lattimorethomas@gmail.com>
2013-10-10 07:33:53 -0400

    Remove unused service.

--- js/services.js
+++ js/services.js
@@ -1,11 +1,6 @@
 'use strict';
 
 angular.module('congresInfo.services', [])
-  .factory('cmsSettings', function($window) {
-    return {
-      hostName: 'http://json-generator.appspot.com'
-    };
-  })
   .factory('congressSettings', function($window) {
     return {
     	hostName: 'http://www.govtrack.us/api/v2'
